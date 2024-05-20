a888b4bda0c3b498e757c80d278be3662ccd5108
lizwachira <lizwanguwachira@gmail.com>
2024-05-20 14:28:31 +0300

    pipenv test working/ coding along with python as well

---
+++ lib/app.py
@@ -0,0 +1,9 @@
+#lib/app.py
+print("Hello world!")
+print("Hello sun!")
+print("Hello sky!")
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
+print(2 + 2) 
+print(4 * 5)
